12b920291ceb09c7650414e156bb956a117ea9ef
Arup Basak <email.arupbasak@gmail.com>
2024-07-07 16:37:20 +0530

    updated security group and terraform yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,65 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0ad21ae1d0696ad58"  # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = "t2.micro"
-  key_name      = "arup-key-pair"
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+# VPC resource
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
-    Name = "AppServerInstance"
+    Name = "main-vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo amazon-linux-extras install docker -y
-              sudo service docker start
-              sudo usermod -a -G docker ec2-user
-              sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              sudo chmod +x /usr/local/bin/docker-compose
-              EOF
+  tags = {
+    Name = "public-route-table"
+  }
 }
 
-resource "aws_eip" "app_server_eip" {
-  instance = aws_instance.app_server.id
-  domain   = "vpc"
+# Route Table Association
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
+# Security Group
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP inbound traffic"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "SSH from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -42,7 +68,6 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
-    description = "HTTP from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -59,6 +84,40 @@ resource "aws_security_group" "allow_ssh_http" {
   tags = {
     Name = "allow_ssh_http"
   }
+}
+
+# EC2 Instance
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0ad21ae1d0696ad58" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type          = "t2.micro"
+  key_name               = "arup-key-pair"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  subnet_id              = aws_subnet.public.id
+
+  tags = {
+    Name = "AppServerInstance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
+              EOF
+}
+
+# Elastic IP
+resource "aws_eip" "app_server_eip" {
+  instance = aws_instance.app_server.id
+  domain   = "vpc"
+}
+
+# Outputs
+output "allow_ssh_http_id" {
+  value = aws_security_group.allow_ssh_http.id
 }
 
 output "ec2_public_ip" {
